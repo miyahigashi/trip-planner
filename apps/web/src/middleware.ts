08478a40cf0feed01d377103b6ac06d5ddcbfc27
Gen Miyahigashi <miyagen0420@gmail.com>
2025-10-11 13:42:12 +0900

    deploy: first upload

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -3,5 +3,8 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!_next|.*\\..*|favicon.ico).*)"],
+  matcher: [
+    // API や Next の静的ファイル等は除外
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)",
+  ],
 };
